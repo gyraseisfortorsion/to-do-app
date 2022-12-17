175f919745d6316de492badf88fb6f89836c6d69
gyraseisfortorsion <talkybek.a@gmail.com>
2022-12-17 16:17:06 +0600

    Initial Commit

---
+++ to-do-app/to_do_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_appApp.swift
+//  to-do-app
+//
+//  Created by Askarali Talkybek on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct to_do_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
